0024e7c0078a8f6d629fcc597d8235e470e164f6
austinescalante <60208327+austinescalante@users.noreply.github.com>
2020-12-21 15:31:49 -0800

    Extend base hmtl, Base CSS and Navigation Bar

--- core/views.py
+++ core/views.py
@@ -5,10 +5,31 @@ from django.shortcuts import render
 # Return a render template instead of httpresponse
 # Takes request as first argument, second is the template name. Third optional argument is context
 
+# Dictionary
+posts = [
+    {
+        'Name': 'Jake',
+        'EmployeeID': '3452',
+        'Title': 'Doctor',
+        'Date_Hired': 'June 6, 2020'
+    },
+    {
+        'Name': 'Amy',
+        'EmployeeID': '5231',
+        'Title': 'Lawyer',
+        'Date_Hired': 'April 24, 2020'
+    }
+]
+
 
 def home(request):
-    return render(request, 'core/home.html')
+    # create a dictionary called context.Create a key within the dictionary called post, value assigned will be posts dictionary.
+    # Now we can pass context to the render function and pass data into our template to use.
+    context = {
+        'posts': posts
+    }
+    return render(request, 'core/home.html', context)
 
 
 def about(request):
-    return render(request, 'core/about.html')
+    return render(request, 'core/about.html', {'Title': 'About'})
