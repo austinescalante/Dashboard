2162ab343dec415dd65c1c814ea63990694f0572
austinescalante <60208327+austinescalante@users.noreply.github.com>
2021-01-07 15:18:47 -0800

    CRUD operations for patient detail view

--- core/views.py
+++ core/views.py
@@ -58,38 +58,45 @@ class PatientDetailView(DetailView):
 
 class PatientCreateView(LoginRequiredMixin, CreateView):
     model = Patient
-    fields = ['FirstName', 'DOB']
+    fields = ['PatientID', 'PatientSSN', 'FirstName',
+              'LastName', 'Address', 'DOB', 'Sex', 'Author']
 
     def form_valid(self, form):
+        # Take instance and set author = to current logged in user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
 
 class PatientUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Patient
-    fields = ['FirstName', 'DOB']
+    fields = ['PatientID', 'PatientSSN', 'FirstName',
+              'LastName', 'Address', 'DOB', 'Sex', 'Author']
 
     def form_valid(self, form):
+        # Take instance and set author = to current logged in user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
     def test_func(self):
-        # get object, Prevent other users from updating other patients info.
+        # get object, Prevent other users from updating other patients info not created by the original creator
         patient = self.get_object()
-        # if self.request.user == patient.author:
-        # return True
-        # return False
+        if self.request.user == patient.Author:
+            return True
+        return False
 
 # Require a user logged in and Author of view
 
 
 class PatientDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Patient
+    success_url = '/'
 
     def test_func(self):
         # get object, Prevent other users from updating other patients info.
         patient = self.get_object()
-        # if self.request.user == patient.author:
-        # return True
-        # return False
+        if self.request.user == patient.Author:
+            return True
+        return False
 
 
 def about(request):
